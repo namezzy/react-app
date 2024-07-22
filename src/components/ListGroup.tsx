9a1edd3841e450ea1e8cc54f40c3bdae05495b6e
withLevi <zengyin.ssr@gmail.com>
2024-07-22 17:25:37 +0800

    Add condition.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,23 @@
 import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  if (items.length === 0) {
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );
+  }
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        {items.map((item,index) => (
+        {items.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
       </ul>
