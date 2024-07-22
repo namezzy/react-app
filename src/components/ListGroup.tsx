4f16a49a9c21861bb3e651aca21cb52150a741ea
withLevi <zengyin.ssr@gmail.com>
2024-07-22 22:12:45 +0800

    &&.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,12 +5,15 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  const message = items.length === 0 ? <p>No item found</p> : null
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found.</p> : null;
+  };
 
   return (
     <>
       <h1>List</h1>
-      {message}
+      {getMessage()}
+      {items.length === 0 && <p>No item found.</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li key={index}>{item}</li>
