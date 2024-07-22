0b831caaf0fb420a394b8b2e12111ba991bc5ce2
withLevi <zengyin.ssr@gmail.com>
2024-07-22 22:04:48 +0800

    Add ternary operator.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,16 @@
 import { Fragment } from "react/jsx-runtime";
+import Message from "../Message";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
+  const message = items.length === 0 ? <p>No item found</p> : null
+
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 ? <p>No item found</p> : null}
+      {message}
       <ul className="list-group">
         {items.map((item, index) => (
           <li key={index}>{item}</li>
