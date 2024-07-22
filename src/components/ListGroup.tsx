e1886af4e8d6dca9343a6d3daa47d37ca1a34b55
withLevi <zengyin.ssr@gmail.com>
2024-07-22 17:26:27 +0800

    Another code method

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,18 +4,10 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  if (items.length === 0) {
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
-  }
-
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item, index) => (
           <li key={index}>{item}</li>
